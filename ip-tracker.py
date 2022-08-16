36cbb5f805f2dea43bb7a383c9ab259bc01e7b2e
AMH-HC <89603191+AMH-HC@users.noreply.github.com>
2022-08-17 00:10:17 +0630

    Update ip-tracker.py

--- ip-tracker.py
+++ ip-tracker.py
@@ -1,3 +1,6 @@
+import json                                           
+from urllib import request
+
 print("\n\033[1;33;40m MYANMAR IP-TRACKER\n")
 print("\n \033[1;33;40m  ██████   ██████ █████ █████   █████████   ██████   █████ ██████   ██████   █████████   ███████████\n")
 print("\n \033[1;33;40m ░░██████ ██████ ░░███ ░░███   ███░░░░░███ ░░██████ ░░███ ░░██████ ██████   ███░░░░░███ ░░███░░░░░███\n")
@@ -16,14 +19,14 @@ request = request.urlopen(url + ip)
 data_json = json.loads(request.read())
 
 if data_json['status'] == "success":
-        print(f"\n \033[1;33;40m  IP\n :\n \033[1;32;40m {data_json['query']}\n")     
-        print(f"\n \033[1;33;40m Country\n: \n \033[1;32;40m {data_json['country']}\n")
-        print(f"\n \033[1;33;40m Region\n :\n \033[1;32;40m {data_json['regionName']}\n") 
-        print(f"\n \033[1;33;40m City\n :\n \033[1;32;40m {data_json['city']}\n")
-        print(f"\n \033[1;33;40m Latitude\n :\n \033[1;32;40m {data_json['lat']}\n") 
-        print(f"\n \033[1;33;40m Longitude\n :\n \033[1;32;40m {data_json['lon']}\n")
-        print(f"\n \033[1;33;40m ISP\n :\n \033[1;32;40m {data_json['isp']}\n") 
-        print(f"\n \033[1;33;40m Zip\n :\n \033[1;32;40m {data_json['zip']}\n")
-        print(f"\n \033[1;33;40m AS\n :\n \033[1;32;40m {data_json['as']}\n")
+        print(f"\n \033[1;33;40m  IP\n \n \033[1;32;40m {data_json['query']}\n")     
+        print(f"\n \033[1;33;40m Country\n \n \033[1;32;40m {data_json['country']}\n")
+        print(f"\n \033[1;33;40m Region\n \n \033[1;32;40m {data_json['regionName']}\n") 
+        print(f"\n \033[1;33;40m City\n \n \033[1;32;40m {data_json['city']}\n")
+        print(f"\n \033[1;33;40m Latitude\n \n \033[1;32;40m {data_json['lat']}\n") 
+        print(f"\n \033[1;33;40m Longitude\n \n \033[1;32;40m {data_json['lon']}\n")
+        print(f"\n \033[1;33;40m ISP\n \n \033[1;32;40m {data_json['isp']}\n") 
+        print(f"\n \033[1;33;40m Zip\n \n \033[1;32;40m {data_json['zip']}\n")
+        print(f"\n \033[1;33;40m AS\n \n \033[1;32;40m {data_json['as']}\n")
 else:
         print("\n \033[1;31;40m Failed to find the IP informations.\n")
